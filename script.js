a7aed9ede1617b02d41c5aada225ff7ff327744e
ShanyGJ <123657211+ShanyGJ@users.noreply.github.com>
2024-08-14 13:09:38 -0600

    creando repositorio para challenge encriptador

---
+++ script.js
@@ -0,0 +1,56 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+const copiarTexto = document.querySelector(".bt-copiar")
+
+/*La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"*/
+
+function bntEncriptar(){
+    const textoEncriptado = encriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none"
+}
+
+function encriptar(stringEncriptada){
+  let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+  stringEncriptada = stringEncriptada.toLowerCase()
+  
+  for(let i = 0; i < matrizCodigo.length; i++){
+    if(stringEncriptada.includes(matrizCodigo[i][0])){
+        stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+    }
+  }
+  return stringEncriptada
+}
+
+function bntDesencriptar(){
+    const textoDesencriptado = desencriptar(textArea.value)
+    mensaje.value = textoDesencriptado
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none"
+}
+
+function desencriptar(stringDesencriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase()
+    
+    for(let i = 0; i < matrizCodigo.length; i++){
+      if(stringDesencriptada.includes(matrizCodigo[i][1])){
+          stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+      }
+    }
+    return stringDesencriptada
+  }
+
+copiarTexto.addEventListener("click", ()=> {
+    let textoCopiado = mensaje.value;
+    navigator.clipboard.writeText(textoCopiado).then(()=> {
+        console.log("Se copio el texto")
+        })
+    })    
+
+   
